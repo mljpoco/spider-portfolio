cfa3533a12592494476ea97e70ec202a3be42cbe
Scribeguru <pocomitch@gmail.com>
2023-12-17 12:22:06 -0500

    installed scss and react query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from './Components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import './styles.scss';
+import { QueryClientProvider, QueryClient } from 'react-query'
 
-function App() {
+export default function App() {
+
+const queryClient = new QueryClient()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
-
-export default App;
