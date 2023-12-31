067a0405621f7be61ea12d3046682118b0fcd73a
Scribeguru <pocomitch@gmail.com>
2023-12-31 18:11:22 -0500

    enter city logic finished for WeatherComponent

--- src/Components/WeatherComponent.js
+++ src/Components/WeatherComponent.js
@@ -4,18 +4,14 @@ import axios from 'axios';
 
 async function fetchWeather(loc) {
     const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=9d3828c4896b4c85a4e145942232612&q=${loc}`);
+    console.log(res.data);
     return res.data.current;
 }
 
 export default function Weather() {
 
     const [location, setLocation] = useState('auto:ip');
-    const { data: weatherData, isLoading, error } = useQuery('weatherData', () => fetchWeather(location));
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(e);
-    }
+    const { data: weatherData, isLoading, error } = useQuery(['weatherData', location], () => fetchWeather(location));
 
     if (isLoading) {
         return <h1 className='loading'>Loading...</h1>
@@ -27,10 +23,13 @@ export default function Weather() {
     return (
         <figure className='weather-box flex-wrapper'>
             <div>
-                <form>
-                    <label for='city' />
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    setLocation(e.target[0].value)
+                }}>
+                    <label htmlFor='city' />
                     <input name='city' id='city' type='text' placeholder='Enter City' />
-                    <input onClick={(e) => handleSubmit(e)} type='submit' />
+                    <button className='weather-submit' type='submit'>GO</button>
                 </form>
             </div>
             <div>
