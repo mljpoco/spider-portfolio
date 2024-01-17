16ca006925baa953214987796ca8bf6d73c5afd6
Scribeguru <pocomitch@gmail.com>
2024-01-16 20:31:00 -0500

    headlines smaller, scrolltop higher, contact component instead of modal for better mobile functionality

---
+++ src/Components/ContactComponent.js
@@ -0,0 +1,41 @@
+import { useState, useRef, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Contact() {
+
+  const [xState, toggleX] = useState('./x-regular.png');
+  const [count, setCount] = useState(0);
+
+  const messageRef = useRef(null);
+
+  useEffect(() => {
+    setTimeout(function () {
+      messageRef.current.scrollIntoView({behavior: 'smooth'});
+    }, 0);
+  }, []);
+
+  const navigate = useNavigate();
+
+  return (
+    <div className='flex-wrapper contact-page'>
+      <img className='x' src={xState} onMouseOver={() => toggleX('./x-active.png')} onMouseLeave={() => toggleX('./x-regular.png')} onClick={() => navigate(-1)} />
+      <h2 ref={messageRef}>Contact Form</h2>
+      <form action='https://formsubmit.co/mitchpoco@mitchpo.co' method='POST' className='flex-wrapper contact-form'>
+        <label htmlFor='name'>
+          Your Name
+        </label>
+        <input id='name' name='name' required />
+        <label htmlFor='email'>
+          Your Email
+        </label>
+        <input type='email' id='email' name='email' required />
+        <label htmlFor='message'>
+          Your Message
+        </label>
+        <textarea id='message' name='message' maxLength={5000} rows={6} onChange={e => setCount(e.target.value.length)} required />
+        <span className='count'>{count} of 5000 characters.</span>
+        <button className='contact-btn' onMouseDown={(e) => e.preventDefault()}><b>Send</b></button>
+      </form>
+    </div>
+  );
+} 
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,60 +1,22 @@
 import { Route, Routes, Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import News from './NewsComponent';
 import Weather from './WeatherComponent';
 import Astronomy from './AstronomyComponent';
+import Contact from './ContactComponent';
 
 export default function Main() {
 
-  const [isModalHidden, toggleModal] = useState(true);
   const [isFaceHidden, hideFace] = useState(true);
-  const [xState, toggleX] = useState('./x-regular.png');
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    document.addEventListener('keydown', detectKeyDown, true);
-  }, []);
-
-  const detectKeyDown = (e) => {
-    if (e.key === 'Escape') {
-      toggleModal(true);
-    }
-  }
-
-  function dim() {
-    return isModalHidden ? 'main-flex-wrapper' : 'main-flex-wrapper dim';
-  }
 
   return (
     <>
-      <div className='modal-wrapper' hidden={isModalHidden}>
-        <div className='flex-wrapper modal'>
-          <img className='x' onClick={() => toggleModal(true)} src={xState} onMouseOver={() => toggleX('./x-active.png')} onMouseLeave={() => toggleX('./x-regular.png')} />
-          <h2>Contact Form</h2>
-          <form action='https://formsubmit.co/mitchpoco@mitchpo.co' method='POST' className='flex-wrapper contact-form'>
-            <label htmlFor='name'>
-              Your Name
-            </label>
-            <input id='name' name='name' required />
-            <label htmlFor='email'>
-              Your Email
-            </label>
-            <input type='email' id='email' name='email' required />
-            <label htmlFor='message'>
-              Your Message
-            </label>
-            <textarea id='message' name='message' maxLength={5000} rows={6} onChange={e => setCount(e.target.value.length)} required />
-            <span className='count'>{count} of 5000 characters.</span>
-            <button className='contact-btn' onMouseDown={(e) => e.preventDefault()}><b>Send</b></button>
-          </form>
-        </div>
-      </div>
-      <main className={dim()}>
+      <main className='main-flex-wrapper'>
         <div className='personal dark'>
           <header className='flex-wrapper light'>
             <span className='name'>Mitchell Poco</span>
             <span className='titles'>Software Developer, Author</span>
-            <a onMouseEnter={() => hideFace(false)} onMouseLeave={() => hideFace(true)} onClick={() => toggleModal(false)} className='contact'>Contact Me</a>
+            <Link to='/contact' onMouseEnter={() => hideFace(false)} onMouseLeave={() => hideFace(true)} className='contact'>Contact Me</Link>
           </header>
           <figure className='grid-container'>
             <a href='https://rockstarelite.xyz' target='_blank'><img src='./rse-thumb.png' /></a>
@@ -72,6 +34,7 @@ export default function Main() {
             <Route exact path='/' element={<News />} />
             <Route path='/weather' element={<Weather />} />
             <Route path='/astronomy' element={<Astronomy />} />
+            <Route path='/contact' element={<Contact />} />
           </Routes>
           <footer className='flex-wrapper dark'>
             <Link to='/' >News</Link>
